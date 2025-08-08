edecf3e49cb5d6345ecac38db894422e13aa42ab
jarrenseson <jarrenys@hawaii.edu>
2025-08-07 21:28:58 -1000

    integrates gemini api

--- redditBot.py
+++ redditBot.py
@@ -1,16 +1,21 @@
 import os
 from dotenv import load_dotenv
 import praw
+from google import genai
+
 
 load_dotenv()
 
+# Reddit Creds
 USERNAME = os.getenv("REDDIT_USERNAME")  # Changed to avoid Windows USERNAME conflict
 PASSWORD = os.getenv("PASSWORD")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 CLIENT_ID = os.getenv("CLIENT_ID")
 
-print(repr(USERNAME), repr(CLIENT_ID), repr(CLIENT_SECRET), repr(PASSWORD))
+# Gemini Creds
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
+# Reddit Instance
 redditInstance = praw.Reddit(
     client_id=CLIENT_ID,
     client_secret=CLIENT_SECRET,
@@ -19,15 +24,23 @@ redditInstance = praw.Reddit(
     user_agent="redditBot by /u/Turbulent_Estate7372"
 )
 
+# Gemini Instance
+gemini = genai.Client()
+
+title = gemini.models.generate_content(
+    model="gemini-2.5-flash", contents="Give me a single eyecatching title for a Reddit post about random fun facts about anything. Give just a title, no other nonsense in your response"
+)
+
+body = gemini.models.generate_content(
+    model="gemini-2.5-flash", contents=f"Write an interesting fun fact that only a very small percentage of people know about. Make it suitable for a Reddit post. The fact should be interesting and engaging, and it should be something that would spark curiosity or discussion among readers. Keep it concise and to the point."
+)
+
+print(f"Gemini title: {title.text}")
+print(f"Gemini Body: {body.text}")
+# Setting subreddit to bot test subreddit
 subreddit = redditInstance.subreddit("testingground4bots")
 try:
-    submission = subreddit.submit(title="Test Submission", selftext="This is a test submission for the bot.")
+    submission = subreddit.submit(title=title.text, selftext=body.text)
     print(f"Submission created: {submission.url}")
 except Exception as e:
-    print(f"Error fetching submissions: {e}")
-
-# # prints logged in user
-# try:
-#     print(redditInstance.user.me())
-# except Exception as e:
-#     print(f"Error fetching user: {e}")
+    print(f"Error posting: {e}")
